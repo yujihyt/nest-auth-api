5a9ba49e5c552c08a1ac7664387bf715124b943b
yujihyt <yuji_teixeira@hotmail.com>
2023-03-03 17:38:53 -0300

    update user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,9 +5,20 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  created: Date;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  modified: Date;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  last_login: Date;
+
   @Column()
   email: string;
 
   @Column()
   password: string;
+
+  token: string;
 }
